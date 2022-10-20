f1fe1ddcac960f496fae7480cd5d38b4750e82e7
edgarilinet <krozeedgar@gmail.com>
2022-10-20 23:19:59 +0500

    Initial commit

--- src/main.java
+++ src/main.java
@@ -5,56 +5,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class main {
-
-    /*public static void main(String[] args) {
-        float vvod1 = 0, vvod2 = 0, result = 0;
-        String deistvie = "0";
-        boolean testDeistvie = false;
-        System.out.println("Hello, i'm Calc");
-        Scanner sc = new Scanner(System.in);
-        vvod1 = sc.nextFloat();
-        deistvie = proverkaZnakaDeistvie(deistvie, testDeistvie, sc);
-        vvod2 = sc.nextFloat();
-        switch (deistvie) {
-            case ("+") -> result = vvod1 + vvod2;
-            case ("-") -> result = vvod1 - vvod2;
-            case ("*") -> result = vvod1 * vvod2;
-            case ("/") -> result = vvod1 / vvod2;
-        }
-        if (vvod1 != 0 & vvod2 == 0 & deistvie.equals("/")){
-            System.out.println("Na 0 delit' nelzya");
-        } else if (vvod1 == 0 & vvod2 == 0 & deistvie.equals("/")){
-            result = 1;
-            System.out.println(result);
-        } else {
-            System.out.println(result);
-        }
-
-    }
-    public static boolean znakDeistvie(String deistvie) {
-        boolean testDeistvie;
-        if (deistvie.equals("+") | deistvie.equals("-") | deistvie.equals("*") | deistvie.equals("/")){
-            testDeistvie = true;
-            //System.out.println("1"+testDeistvie);
-        } else {
-            testDeistvie = false;
-            //System.out.println("2"+testDeistvie);
-        }
-        return testDeistvie;
-    }
-    }*/
-
     public static void main(String[] args) {
-        /*String vvod;
-        System.out.println("Hello i'm calk vvodi primer");
-        Scanner sc = new Scanner(System.in);
-        vvod = sc.nextLine();
-        String[] numbers =   vvod.split("[-+/*]");
-        int[] num = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            num[i] = Integer.parseInt(numbers[i]);
-        }
-        System.out.println(Arrays.toString(num));*/
+
         Scanner sc = new Scanner(System.in);
         String vvod = "-5+4*(44-5)";
         vvod = sc.nextLine();
