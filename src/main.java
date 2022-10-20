86d6792d76003a1545d44301dbc9c06c7fa0f526
edgarilinet <krozeedgar@gmail.com>
2022-10-20 23:18:22 +0500

    Initial commit

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class main {
 
@@ -42,7 +45,7 @@ public class main {
     }*/
 
     public static void main(String[] args) {
-        String vvod;
+        /*String vvod;
         System.out.println("Hello i'm calk vvodi primer");
         Scanner sc = new Scanner(System.in);
         vvod = sc.nextLine();
@@ -51,14 +54,61 @@ public class main {
         for (int i = 0; i < numbers.length; i++) {
             num[i] = Integer.parseInt(numbers[i]);
         }
-        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(num));*/
+        Scanner sc = new Scanner(System.in);
+        String vvod = "-5+4*(44-5)";
+        vvod = sc.nextLine();
+        Pattern pattern = Pattern.compile("[+\\-*/=()]");
+        Matcher matcher = pattern.matcher(vvod);
+        ArrayList<String> operator = new ArrayList<>();
+        ArrayList<String> operand = new ArrayList<>();
+        int prev = 0, i = 0;
+        String last = null;
+        while(matcher.find()) {
+            operator.add(i, vvod.substring(matcher.start(), matcher.end()));
+            operand.add(i, vvod.substring(prev, matcher.start()));
+            System.out.printf("Num: '%s', znak: '%s'\n", operand.get(i), operator.get(i));
+            prev = matcher.end();
+            i++;
+        }
+        if(prev > 0 && prev < vvod.length()) {
+           last = vvod.substring(prev, vvod.length());
+            System.out.printf("last: %s\n", last);
+        }
+        //блок математики
+        float sum = Float.parseFloat(operand.get(0));
+        for(int f = 0; f < operand.size(); f++){
+            if (operator.get(f).equals("+")) {
+                try {
+                    sum += Float.parseFloat(operand.get(f+1));
+                } catch (IndexOutOfBoundsException exc){
+                    sum += Float.parseFloat(last);
+                }
+            }
+            if (operator.get(f).equals("-")) {
+                try {
+                    sum -= Float.parseFloat(operand.get(f+1));
+                } catch (IndexOutOfBoundsException exc){
+                    sum -= Float.parseFloat(last);
+                }
+            }
+            if (operator.get(f).equals("*")) {
+                try {
+                    sum *= Float.parseFloat(operand.get(f+1));
+                } catch (IndexOutOfBoundsException exc){
+                    sum *= Float.parseFloat(last);
+                }
+            }
+            if (operator.get(f).equals("/")) {
+                try {
+                    sum /= Float.parseFloat(operand.get(f+1));
+                } catch (IndexOutOfBoundsException exc){
+                    sum /= Float.parseFloat(last);
+                }
+            }
 
+        }
+        System.out.println(sum);
+//        System.out.println(1+5*2);
     }
-/*    public static int[] allNumbers(String vvod){
-
-        System.out.println(num);
-        return;
-    }*/
-
-
 }
